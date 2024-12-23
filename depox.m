3e05b465a33ff05e4169f786936313ebca2c9f4d
zhufucdev <zhufuzhufu1@gmail.com>
2024-12-23 22:04:32 +0800

    :sparkles: support pox mask output

--- depox.m
+++ depox.m
@@ -1,4 +1,4 @@
-function F = depox(im, mask, blur_strength, batch_size)
+function [F, M] = depox(im, mask, blur_strength, batch_size)
     if ~exist('blur_strength', 'var')
         blur_strength = 20;
     end
@@ -8,13 +8,13 @@ function F = depox(im, mask, blur_strength, batch_size)
     
 
     kernel_isolated_points = [-1 -1 -1; -1 8 -1; -1 -1 -1];
-    basic_pox = abs(double(imfilter(im2gray(im2double(im)), kernel_isolated_points)) .* mask);
+    basic_pox = abs(double(imfilter(im2gray(im), kernel_isolated_points)) .* mask);
     basic_pox = rescale(basic_pox, 0, 1);
 
     kernel_has_neighbour = ones(batch_size);
-    pox_grown = imfilter(basic_pox, kernel_has_neighbour);
-    pox_grown = rescale(pox_grown, 0, 1);
+    M = imfilter(basic_pox, kernel_has_neighbour);
+    M = rescale(M, 0, 1);
 
-    gaussined = imgaussfilt(im2double(im), blur_strength);
-    F = gaussined .* pox_grown + im2double(im) .* (1 - pox_grown);
+    gaussined = imgaussfilt(im, blur_strength);
+    F = gaussined .* M + im .* (1 - M);
 end
--- test.m
+++ test.m
@@ -1,4 +1,4 @@
-s1 = imread('sample1.jpg');
+s1 = imread('sample4.jpg');
 
 [F, B] = facial(s1);
 
@@ -8,6 +8,9 @@ imshow(s1);
 subplot(1, 4, 2);
 imshow(F);
 
+[depoxed, pox_mask] = depox(im2double(s1), F);
 subplot(1, 4, 3);
-depoxed = depox(s1, F);
+imshow(im2double(s1) .* pox_mask);
+
+subplot(1, 4, 4);
 imshow(depoxed);
